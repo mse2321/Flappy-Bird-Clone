f2925dbc8c41b6f64c29190005dc760cb06c1270
mse2321 <mse233@gmail.com>
2015-12-16 16:57:58 -0500

    starting to build game

--- js/components/graphics/bird.js
+++ js/components/graphics/bird.js
@@ -1,19 +1,16 @@
-console.log("bird graphics are working");
-
 var BirdGraphicsComponent = function(entity) {
     this.entity = entity;
 };
 
-BirdGraphicsComponent.prototype.draw = function(context, position, size) {
-    var position = {x: 0, y: 0.5};
+BirdGraphicsComponent.prototype.draw = function(context) {
+    var position = this.entity.components.physics.position;
 
-    console.log("Drawing a bird");
     context.save();
     context.translate(position.x, position.y);
-    context.scale(size, size);
     context.beginPath();
-    context.arc(0, 0, 1, 0, 2 * Math.PI);
+    context.arc(0, 0, 0.02, 0, 2 * Math.PI);
     context.fill();
+    context.closePath();
     context.restore();
 };
 
--- js/flappy_bird.js
+++ js/flappy_bird.js
@@ -1,16 +1,24 @@
 console.log("flappy bird is working");
 
 var graphicsSystem = require('./systems/graphics');
-var bird = require('./entities/bird');
-var pipe = require('./entities/pipe');
+var physicsSystem = require('./systems/systems_physics');
+var inputSystem = require('./systems/input');
+
+var bird = require('./entities/entities_bird');
+/* var pipe = require('./entities/entities_pipe'); */
 
 var FlappyBird = function() {
-    this.entities = [new bird.Bird(), new pipe.Pipe()];
+	this.entities = [new bird.Bird()];
+    /* this.entities = [new bird.Bird(), new pipe.Pipe()]; */
     this.graphics = new graphicsSystem.GraphicsSystem(this.entities);
+    this.physics = new physicsSystem.PhysicsSystem(this.entities);
+    this.input = new inputSystem.InputSystem(this.entities);
 };
 
 FlappyBird.prototype.run = function() {
     this.graphics.run();
+    this.physics.run();
+    this.input.run();
 };
 
 /* main.js */
