4a9bb2992d8c5211f6174e515ea3e7e06046c8f5
mse2321 <mse233@gmail.com>
2016-01-01 14:43:13 -0500

    reverted back to old changes to start over

--- js/components/graphics/pipe.js
+++ js/components/graphics/pipe.js
@@ -2,6 +2,11 @@ console.log("pipe graphics are working");
 
 var PipeGraphicsComponent = function(entity) {
     this.entity = entity;
+
+    this.size = {
+        x: 0.1,
+        y: 0.5
+    };
 };
 
 /*
@@ -19,16 +24,24 @@ PipeGraphicsComponent.prototype.draw = function(context) {
 
 */
 
-PipeGraphicsComponent.prototype.draw = function(context) {
+PipeGraphicsComponent.prototype.draw = function(context, size) {
     var position = this.entity.components.physics.position;
 
     context.save();
-    context.translate(position.x, position.y);
-    context.beginPath();
+    //context.translate(position.x, position.y);
+    context.translate(0.8, 0.8);
     context.fillStyle = "green";
-    context.fillRect(0, 0, 0.1, 0.4);
-    context.closePath();
+    context.fillRect(-this.size.x / 2, -this.size.y / 2, this.size.x, this.size.y);
     context.restore();
+    console.log("pipe1 graphics have been drawn");
+
+    context.save();
+    //context.translate(position.x - 0.4, position.y - 0.6);
+    context.translate(0.4, 0.2);
+    context.fillStyle = "green";
+    context.fillRect(-this.size.x / 2, -this.size.y / 2, this.size.x, this.size.y);
+    context.restore();
+    console.log("pipe2 graphics have been drawn");
 };
 
 exports.PipeGraphicsComponent = PipeGraphicsComponent;
